5a17076122ee3574639c097339d812c5d811a031
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-08-05 10:29:28 +0200

    Basic example main menu

---
+++ example/MainMenu.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+func _ready() -> void:
+	pass
+
+
+func _on_start_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
