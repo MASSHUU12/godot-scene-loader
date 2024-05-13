fda7adf0b810b095c7cb29e42577feabf04b5992
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-05-13 17:43:47 +0200

    Fix #11

--- addons/scene_loader/autoloads/scene_loader.gd
+++ addons/scene_loader/autoloads/scene_loader.gd
@@ -2,15 +2,14 @@ extends Node
 
 var scenes: Dictionary = {}
 var path_to_progress_bar: String = "Container/ProgressBar"
-var loading_screen: Resource  = preload("res://addons/scene_loader/default_loading_screen/DefaultLoadingScreen.tscn")
+var loading_screen: Resource = preload ("res://addons/scene_loader/default_loading_screen/DefaultLoadingScreen.tscn")
 
 enum ThreadStatus {
-	INVALID_RESOURCE = 0, # THREAD_LOAD_INVALID_RESOURCE
-	IN_PROGRESS = 1, # THREAD_LOAD_IN_PROGRESS
-	FAILED = 2, # THREAD_LOAD_FAILED
-	LOADED = 3 # THREAD_LOAD_LOADED
+    INVALID_RESOURCE = 0, # THREAD_LOAD_INVALID_RESOURCE
+    IN_PROGRESS = 1, # THREAD_LOAD_IN_PROGRESS
+    FAILED = 2, # THREAD_LOAD_FAILED
+    LOADED = 3 # THREAD_LOAD_LOADED
 }
-
 
 ## Sets the configuration for the scene loader.
 ##
@@ -22,15 +21,14 @@ enum ThreadStatus {
 ##
 ## Returns: None
 func set_configuration(config: Dictionary) -> void:
-	if config.has("scenes"):
-		scenes = config["scenes"]
+    if config.has("scenes"):
+        scenes = config["scenes"]
 
-	if config.has("path_to_progress_bar"):
-		path_to_progress_bar = config["path_to_progress_bar"]
+    if config.has("path_to_progress_bar"):
+        path_to_progress_bar = config["path_to_progress_bar"]
 
-	if config.has("loading_screen"):
-		loading_screen = load(config["loading_screen"])
-
+    if config.has("loading_screen"):
+        loading_screen = load(config["loading_screen"])
 
 ## Loads a scene asynchronously and replaces the current scene with it.
 ##
@@ -40,46 +38,43 @@ func set_configuration(config: Dictionary) -> void:
 ##
 ## Returns: None
 func load_scene(current_scene: Node, next_scene: String) -> void:
-	var loading_screen_instance: Node = initialize_loading_screen()
-	var path: String = find_scene_path(next_scene)
+    var loading_screen_instance: Node = initialize_loading_screen()
+    var path: String = find_scene_path(next_scene)
 
-	# Load scene
-	if ResourceLoader.load_threaded_request(path) != OK:
-		printerr("Scene %s does not exist." % path)
-		return
+    # Load scene
+    if ResourceLoader.load_threaded_request(path) != OK:
+        printerr("Scene %s does not exist." % path)
+        return
 
-	# Wait for loading screen to be ready
-	await loading_screen_instance.safe_to_load
-	current_scene.queue_free()
+    # Wait for loading screen to be ready
+    await loading_screen_instance.safe_to_load
+    current_scene.queue_free()
 
-	while true:
-		var load_progress = []
-		var load_status = ResourceLoader.load_threaded_get_status(path, load_progress)
+    while true:
+        var load_progress = []
+        var load_status = ResourceLoader.load_threaded_get_status(path, load_progress)
 
-		match load_status:
-			ThreadStatus.INVALID_RESOURCE:
-				printerr("Can not load the resource.")
-				return
-			ThreadStatus.IN_PROGRESS:
-				update_progress_bar(loading_screen_instance, path_to_progress_bar, load_progress[0])
-				return
-			ThreadStatus.FAILED:
-				printerr("Loading failed.")
-				return
-			ThreadStatus.LOADED:
-				load_next_scene(path, loading_screen_instance)
-				return
-
+        match load_status:
+            ThreadStatus.INVALID_RESOURCE:
+                printerr("Can not load the resource.")
+                return
+            ThreadStatus.IN_PROGRESS:
+                update_progress_bar(loading_screen_instance, path_to_progress_bar, load_progress[0])
+            ThreadStatus.FAILED:
+                printerr("Loading failed.")
+                return
+            ThreadStatus.LOADED:
+                load_next_scene(path, loading_screen_instance)
+                return
 
 ## Loads a scene asynchronously and adds it to the current scene.
 ##
 ## Returns: Loading screen instance
 func initialize_loading_screen() -> Node:
-	var loading_screen_instance: Node = loading_screen.instantiate()
-	get_tree().get_root().call_deferred("add_child", loading_screen_instance)
+    var loading_screen_instance: Node = loading_screen.instantiate()
+    get_tree().get_root().call_deferred("add_child", loading_screen_instance)
 
-	return loading_screen_instance
-
+    return loading_screen_instance
 
 ## Finds the path to the scene file.
 ##
@@ -88,16 +83,15 @@ func initialize_loading_screen() -> Node:
 ##
 ## Returns: The path to the scene file.
 func find_scene_path(next_scene: String) -> String:
-	# Find path to the scene file
-	var path: String = scenes[next_scene] if scenes.has(next_scene) else next_scene
+    # Find path to the scene file
+    var path: String = scenes[next_scene] if scenes.has(next_scene) else next_scene
 
-	# Validate path
-	if not ResourceLoader.exists(path):
-		printerr("Scene %s does not exist." % path)
-		return ""
+    # Validate path
+    if not ResourceLoader.exists(path):
+        printerr("Scene %s does not exist." % path)
+        return ""
 
-	return path
-
+    return path
 
 ## Updates the progress bar in the loading screen.
 ##
@@ -108,16 +102,15 @@ func find_scene_path(next_scene: String) -> String:
 ##
 ## Returns: None
 func update_progress_bar(loading_screen_instance: Node, path_to_progress_bar: String, load_progress: int) -> void:
-	if path_to_progress_bar == "":
-		return
+    if path_to_progress_bar == "":
+        return
 
-	var progress_bar := loading_screen_instance.get_node(path_to_progress_bar)
+    var progress_bar := loading_screen_instance.get_node(path_to_progress_bar)
 
-	if progress_bar == null:
-		printerr("Path to progress bar is invalid.")
-	else:
-		progress_bar.value = load_progress
-
+    if progress_bar == null:
+        printerr("Path to progress bar is invalid.")
+    else:
+        progress_bar.value = load_progress
 
 ## Loads the next scene.
 ##
@@ -127,6 +120,6 @@ func update_progress_bar(loading_screen_instance: Node, path_to_progress_bar: St
 ##
 ## Returns: None
 func load_next_scene(path: String, loading_screen_instance: Node) -> void:
-	var next_scene_instance = ResourceLoader.load_threaded_get(path).instantiate()
-	get_tree().get_root().call_deferred("add_child", next_scene_instance)
-	loading_screen_instance.loading_finished.emit()
+    var next_scene_instance = ResourceLoader.load_threaded_get(path).instantiate()
+    get_tree().get_root().call_deferred("add_child", next_scene_instance)
+    loading_screen_instance.loading_finished.emit()
