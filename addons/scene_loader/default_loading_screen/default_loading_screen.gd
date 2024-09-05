f60be612c84e1ef519a746308ca914c5beb32140
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-09-05 16:35:34 +0200

    feat: move loading_finished signal from loading screen to SceneLoader

--- addons/scene_loader/default_loading_screen/default_loading_screen.gd
+++ addons/scene_loader/default_loading_screen/default_loading_screen.gd
@@ -1,16 +1,15 @@
 extends CanvasLayer
 
 signal safe_to_load
-signal loading_finished
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _ready() -> void:
-	loading_finished.connect(fade_out_loading_screen)
+	SceneLoader.loading_finished.connect(fade_out_loading_screen)
 
 
-func fade_out_loading_screen() -> void:
+func fade_out_loading_screen(status: SceneLoader.ThreadStatus) -> void:
 	animation_player.play("fade_out")
 	await animation_player.animation_finished
 	queue_free()
